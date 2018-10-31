b65c0e3c0c57f8baa15d66546bb69ed4b1723c1a
Thobias <thobiashorst@hotmail.com>
2018-10-31 11:10:12 +0100

    website front end

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+    require_once "inc/package.inc.php";
+    require('inc/config.php');
+
+    $view = "views/index.php";
+    $sectionActive = "Home";
+
+    include_once $template;
+?>
---
+++ views/shop.php
@@ -0,0 +1,11 @@
+<div class="container">
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
+</div>
