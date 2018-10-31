8523252b4e827bb6dccde8656450af713c9e61d1
Thobias <thobiashorst@hotmail.com>
2018-10-31 11:08:28 +0100

    website setup afgemaakt

--- inc/config.php
+++ inc/config.php
@@ -8,11 +8,12 @@
 
     // //database credentials
     define('DBHOST','localhost');
-    define('DBNAME','workspace');
+    define('DBNAME','wideworldimporters');
     define('DBUSER','root');
     define('DBPASS','');
 
-    //$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
-    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
 
 ?>
