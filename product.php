d4a0815d0fa4e78ef1b5caf04d5cab7537752224
Patrick <patrick@rienstra.biz>
2018-11-02 13:58:43 +0100

    fixed session error

--- product.php
+++ product.php
@@ -1,19 +1,12 @@
 <link rel="stylesheet" href="css/shop.css">
 <?php
 require_once "inc/package.inc.php";
-require('shop.php');
+require('inc/config.php');
 
 $view = "views/product.php";
 $sectionActive = "product";
-$idt=1;
+
 include_once $template;
 
-$query="
-SELECT s.stockitemname, s.stockitemid, s.brand, s.size, s.leadtimedays, s.ischillerstock, s.taxrate, s.unitprice, s.marketingcomments, s.photo, s.customfields, c.colorname, f.quantityonhand
-FROM stockitems s
-JOIN colors c ON s.colorid = c.colorid
-JOIN stockitemholdings f ON s.stockitemid = f.stockitemid
-WHERE s.StockItemID = $idt";
 
-$stmt = $db->prepare($query);
 ?>
--- views/product.php
+++ views/product.php
@@ -1,12 +1,31 @@
 <?php
-if ($stmt -> execute()){
+
+$query="
+SELECT stockitemname, s.stockitemid, brand, size, leadtimedays, ischillerstock, taxrate, unitprice, marketingcomments, photo, customfields, colorname, quantityonhand
+FROM stockitems s
+LEFT JOIN colors c ON s.colorid = c.colorid
+JOIN stockitemholdings f ON s.stockitemid = f.stockitemid
+WHERE s.StockItemID = 199";
+
+$stmt = $db->prepare($query);
+if($stmt->execute()){
+    print("succeed");
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        print('succes');
+        print($row["stockitemname"]);
         ?>
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-lg-12 col-md-12">
             <div class="thumbnail">
-                <img class="productimg" src=<?php echo $row["photo"] ?>>
+                <?php
+                if($row["photo"] == ""){
+                    ?>
+                    <img class="productimg" src="https://www.britax-roemer.nl/on/demandware.static/Sites-Britax-EU-Site/-/default/dwf9277f59/images/britax/PlaceholderProductImage.jpg" width="500" height="500">
+                    <?php
+                }
+                ?>
+                <img class="productimg" src=<?php echo $row["photo"]; ?>>
                 <div class="caption">
                     <h4 class="pull-right"><?php echo $row["unitprice"]; ?></h4>
                     <h4><?php echo $row["stockitemname"]; ?></h4>
@@ -15,7 +34,6 @@ if ($stmt -> execute()){
                     <p><?php echo $row["leadtimedays"]; ?></p>
                     <p><?php echo $row["ischillerstock"]; ?></p>
                     <p><?php echo $row["taxrate"]; ?></p>
-                    <p><?php echo $row["unitprice"]; ?></p>
                     <p><?php echo $row["marketingcomments"]; ?></p>
                     <p><?php echo $row["customfields"]; ?></p>
                     <p><?php echo $row["colorname"]; ?></p>
