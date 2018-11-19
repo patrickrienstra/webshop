c7f02c26016a0afee8d202c7329c09aa7a1848aa
Patrick <patrick@rienstra.biz>
2018-11-19 15:35:44 +0100

    Revert "Merge pull request #4 from patrickrienstra/webshoppie"
    This reverts commit 68dfd7a6a89a05d6e098679ccd1cbeafbfa4c13f, reversing
    changes made to b1587b5fa5aaf9d874cc2c22ab958ce9fa717ab6.
    
    Reverting 1 commit/merge

--- shop.php
+++ shop.php
@@ -2,14 +2,7 @@
 <?php
     require_once "inc/package.inc.php";
     require('inc/config.php');
-    
-    $query = "SELECT s.stockitemid, s.stockitemname, s.brand, s.unitprice, s.photo, f.quantityonhand
-    FROM stockitems s
-    JOIN stockitemholdings f 
-    ON s.stockitemid = f.stockitemid";
-    $query_prepare = $db->prepare($query);
-    if($query_prepare->execute()) {
-    }
+
     $view = "views/shop.php";
     $sectionActive = "Shop";
 
--- views/shop.php
+++ views/shop.php
@@ -1,104 +1,11 @@
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-		<img class="workshop" src="http://placehold.it/320x150">
-	        <div class="caption">
-                    <h4 class="pull-right">prijs</h4>
-                    <h4><a href="form.php?id=1">naam product</a></h4>
-                    <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-		</div>
-            </div>
-	</div>
-    </div>
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
+        <div class="col-md-4"> 
+            <p>hey</p>
+        </div>
 </div>
