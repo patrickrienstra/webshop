dae9c31187411ef6cf1a101fa2941a6238356ce3
Raviinl <raviramezani98@gmail.com>
2018-12-03 12:20:49 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,6 @@
 <div class="footer">
     <div class="content-footer">
         <ul class="footer-links">
-            <li class="footer-link"><a href="/contact">Contact</a> </li>
-            <li class="footer-link"><a class="footer-social-link" target="_blank" href="">Google</a> </li>
-            <li class="footer-link"><a class="footer-social-link" target="" href="login.php">Login</a> </li>
         </ul>
     </div>
 </div>
