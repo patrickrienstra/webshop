ac22ef7c50a4c20a199a89550257baf20c995cb3
Patrick <patrick@rienstra.biz>
2018-12-05 09:08:32 +0100

    file move

---
+++ views/orderConfirmation.php
@@ -0,0 +1,9 @@
+<div class="login-page">
+    <div class="form">
+        <div class="login-form">
+            <a>Uw bestelling is verwerkt</a><br>
+            <a>Uw bestelnummer is <?php echo /*$var*/;?></a>
+        </div>
+    </div>
+</div>
+
